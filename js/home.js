1bb9fe012387d0cc1fc8af399dd9b1eaca597792
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-08 12:51:46 -0700

    Debugging

--- js/home.js
+++ js/home.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 	$.post('http://ferrero.x10host.com/index.php', {
 		'type': 0
 	}, function(response) {
+		console.log(reponse);
 		var data = JSON.parse(response);
+		console.log(data);
 		for (var name in data) {
 			$('body').append('<h1>' + name + '</h1>');
 			$('body').append('<p>' + data[name] + '</p>');
