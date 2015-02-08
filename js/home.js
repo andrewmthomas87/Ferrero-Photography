0b82f8e604ca7ea067b4014d60125947afc3dd61
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-08 12:37:44 -0700

    Reverted to original POST call

--- js/home.js
+++ js/home.js
@@ -1,15 +1,9 @@
 
 $(document).ready(function() {
-	$.ajax({
-		url: 'http://ferrero.x10host.com/index.php',
-		data: 1,
-		type: 'GET',
-		crossDomain: true,
-		success: function() {
-			alert('Success');
-		},
-		error: function() {
-			alert('Failure');
-		}
+	$.post('http://ferrero.x10host.com/index.php', {
+		'type': 0
+	}, function(data) {
+		console.log(data);
+		$('h1').html(data);
 	});
 });
