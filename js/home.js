01cbde32cffd6a5b5cc4c94a46cd1f0f8c8dcb13
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-08 12:57:27 -0700

    Changed variable name

--- js/home.js
+++ js/home.js
@@ -2,9 +2,9 @@
 $(document).ready(function() {
 	$.post('http://ferrero.x10host.com/get.php', {
 		'type': 0
-	}, function(response) {
-		console.log(reponse);
-		var data = JSON.parse(response);
+	}, function(data) {
+		console.log(data);
+		var data = JSON.parse(data);
 		console.log(data);
 		for (var name in data) {
 			$('body').append('<h1>' + name + '</h1>');
