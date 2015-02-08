c7d6647f4b2ba919efa5d09e52b1e59451e9038f
Andrew Thomas <andrewmthomas87@gmail.com>
2015-02-08 12:59:15 -0700

    Removed debugging

--- js/home.js
+++ js/home.js
@@ -3,9 +3,7 @@ $(document).ready(function() {
 	$.post('http://ferrero.x10host.com/get.php', {
 		'type': 0
 	}, function(data) {
-		console.log(data);
 		var data = JSON.parse(data);
-		console.log(data);
 		for (var name in data) {
 			$('body').append('<h3>' + name + '</h3>');
 			$('body').append('<p>' + data[name] + '</p>');
